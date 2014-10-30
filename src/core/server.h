92c28e27a3e8b9811e8e22f01beee8ccdfeef7d5
Eric Lavesson <eric.lavesson@gmail.com>
2014-10-30 01:25:13 +0100

    Server is non-copyable with private implementation

--- src/core/server.h
+++ src/core/server.h
@@ -1,9 +1,18 @@
 #ifndef	APIMOCK_CORE_SERVER_H
 #define APIMOCK_CORE_SERVER_H
 #include <string>
+#include <memory>
 
 class Server {
+	struct ServerImpl;
+	std::unique_ptr<ServerImpl> _impl;
+
+	Server& operator=(const Server&);
+	Server(const Server&);
+
 public:
+	Server();
+	~Server();
 	void startServer(std::string address, int port);
 };
 
