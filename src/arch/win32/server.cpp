1b68f571fa0f315e06fdaccf9b9ff275afd33988
Eric Lavesson <eric.lavesson@gmail.com>
2014-10-30 01:53:49 +0100

    Fire up WinSock

--- src/arch/win32/server.cpp
+++ src/arch/win32/server.cpp
@@ -1,4 +1,33 @@
 #include "core/server.h"
+#include "core/exceptions.h"
+#include <windows.h>
+#include <winsock.h>
+
+#define VERSION_WINSOCK2 0x0202
+#pragma comment(lib, "wsock32.lib")
+
+struct Server::ServerImpl {
+	SOCKET s;
+	WSADATA w;
+
+	void listen(int port) {
+		auto err = WSAStartup(VERSION_WINSOCK2, &w);
+		
+		if (err) 
+			throw SocketException("Something went wrong when starting WinSocks. The error code was: " + std::to_string(err) + "\n");
+
+		if (w.wVersion != VERSION_WINSOCK2)
+			throw SocketException("Expected WinSock 2.0, but the reported version was: " + std::string(w.szDescription) + "\n");
+	}
+
+	~ServerImpl() {
+		WSACleanup();
+	}
+};
+
+Server::Server() : _impl(new ServerImpl) {}
+Server::~Server() {}
 
 void Server::startServer(std::string address, int port) {
+	_impl->listen(80);
 }
