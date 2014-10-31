e352a60b3b0ea4d540eef8e11b2a348dd7fce44c
Eric Lavesson <eric.lavesson@gmail.com>
2014-10-31 01:18:24 +0100

    Use CRLF in requests instead of just LF

--- src/http/requestparser.cpp
+++ src/http/requestparser.cpp
@@ -3,11 +3,12 @@
 #include <vector>
 
 std::vector<std::string> ApiMock::RequestParser::projectToCollection(std::string const& request) {
+	const std::string CRLF = "\r\n";
 	std::vector<std::string> fromRequest;
 	std::string::size_type pos = 0;
 	std::string::size_type prev = 0;
 
-	while ((pos = request.find('\n', prev)) != std::string::npos) {
+	while ((pos = request.find(CRLF, prev)) != std::string::npos) {
 		fromRequest.push_back(request.substr(prev, pos - prev));
 		prev = pos + 1;
 	}
