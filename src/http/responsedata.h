8cb37afde89b4c89079f579f3532792d7dd5ff67
Eric Lavesson <eric.lavesson@gmail.com>
2014-10-31 03:05:23 +0100

    Add headers and status code to response

--- src/http/responsedata.h
+++ src/http/responsedata.h
@@ -2,10 +2,14 @@
 #define APIMOCK_RESPONSEDATA_H
 
 #include <string>
+#include <unordered_map>
+#include "statuscodes.h"
 
 namespace ApiMock {
 	struct ResponseData {
 		std::string body;
+		std::unordered_map<std::string, std::string> headers;
+		HTTP_RESPONSE_CODE statusCode;
 	};
 }
 
