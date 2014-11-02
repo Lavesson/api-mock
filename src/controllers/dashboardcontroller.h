1821af41609ef6c92541e265f2347ca6d944cfba
Eric Lavesson <eric.lavesson@gmail.com>
2014-11-02 03:20:40 +0100

    Add a dashboard controller

---
+++ src/controllers/dashboardcontroller.h
@@ -0,0 +1,12 @@
+#ifndef	APIMOCK_DASHBOARDCONTROLLER_H
+#define APIMOCK_DASHBOARDCONTROLLER_H
+
+#include "controller.h"
+
+namespace ApiMock {
+	class DashboardController : public Controller {
+		
+	};
+}
+
+#endif
