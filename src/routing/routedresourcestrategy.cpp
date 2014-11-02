104234fa8f55222599e42b497216296e52c66939
Eric Lavesson <eric.lavesson@gmail.com>
2014-11-02 03:00:57 +0100

    Kind of non-working routing in place

--- src/routing/routedresourcestrategy.cpp
+++ src/routing/routedresourcestrategy.cpp
@@ -1,6 +1,38 @@
 #include "routedresourcestrategy.h"
+#include "routingtemplate.h"
+#include "controllers/controller.h"
+
+void ApiMock::RoutedResourceStrategy::registerRoute(RoutingTemplate route, Controller* ctrl) {
+	_routeMap.insert(
+		std::make_pair(route, ctrl));
+}
+
+ApiMock::Controller* ApiMock::RoutedResourceStrategy::matchRoute(const std::string& uri) {
+	return nullptr;
+}
+
+ApiMock::ResponseData ApiMock::RoutedResourceStrategy::errorResponse(HTTP_RESPONSE_CODE status) {
+	ResponseData response;
+	response.statusCode = status;
+	response.body = "";
+	response.headers["Connection"] = "Close";
+	return response;
+}
 
 ApiMock::ResponseData ApiMock::RoutedResourceStrategy::createResponse(RequestData const& request) {
+	auto route = matchRoute(request.requestUri);
+
+	if (!route) return errorResponse(HTTP_NOT_FOUND);
+
 	ResponseData r;
+
+	switch (request.method) {
+	case RequestData::GET: route->get(request); break;
+	case RequestData::POST: route->post(request); break;
+	case RequestData::PUT: route->put(request); break;
+	case RequestData::DELETE: route->del(request); break;
+	default: return errorResponse(HTTP_METHOD_NOT_ALLOWED);
+	}
+
 	return r;
 }
--- src/routing/routedresourcestrategy.h
+++ src/routing/routedresourcestrategy.h
@@ -2,10 +2,22 @@
 #define APIMOCK_ROUTEDRESOURCESTRATEGY_H
 
 #include "http/resourcestrategy.h"
+#include "routeregistry.h"
+#include "routingtemplate.h"
+#include <unordered_map>
 
 namespace ApiMock {
-	class RoutedResourceStrategy : public ResourceStrategy {
+	class Controller;
+	enum HTTP_RESPONSE_CODE;
+
+	class RoutedResourceStrategy : 
+		public ResourceStrategy, public RouteRegistry {
+		std::unordered_map<RoutingTemplate, Controller*> _routeMap;
+		ResponseData errorResponse(HTTP_RESPONSE_CODE status);
+		Controller* matchRoute(const std::string& uri);
+
 	public:
+		void registerRoute(RoutingTemplate route, Controller* ctrl) override;
 		ResponseData createResponse(RequestData const& request) override;
 	};
 }
