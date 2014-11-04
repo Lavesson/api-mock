5898c41495a23e5d509e2586321a8af53d3d6aed
Eric Lavesson <eric.lavesson@gmail.com>
2014-11-04 02:11:05 +0100

    Poor man's IoC (or rather, global service resolution)

---
+++ src/ioc/ioc.h
@@ -0,0 +1,26 @@
+#ifndef APIMOCK_IOC_H
+#define	APIMOCK_IOC_H
+
+#include <unordered_map>
+#include <string>
+
+namespace ApiMock {
+	class ServiceRegistry {
+		static std::unordered_map<std::string, void*> container;
+
+	public:
+		template<typename T> 
+		static void Register(T* object) {
+			container[typeid(T).name()] = reinterpret_cast<void*>(object);
+		}
+
+		template<typename T> 
+		static T* Resolve() {
+			return static_cast<T*>(container[typeid(T).name()]);
+		}
+	};
+
+	std::unordered_map<std::string, void*> ServiceRegistry::container;
+};
+
+#endif
