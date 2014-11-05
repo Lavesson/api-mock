754660a6d6ccc0c651aa8a64a3b56f72b1960b2c
Eric Lavesson <eric.lavesson@gmail.com>
2014-11-05 22:34:32 +0100

    Some more routing tests
    These are already implemented things that *should* really have
    been tested right from the start.

--- test/src/routingtests.cpp
+++ test/src/routingtests.cpp
@@ -1,5 +1,5 @@
 #include "t-unit.h"
-#include <routing/routingtemplate.h>
+#include "api-mock.h"
 
 FIXTURE(RoutingTests) {
 	TEST(MatchesNonTemplatedRoute) {
@@ -10,5 +10,12 @@ FIXTURE(RoutingTests) {
 	TEST(MatchesTemplatedRoute) {
 		ApiMock::RoutingTemplate sut("/first/{second}/third");
 		assert.is_true(sut.isMatch("/first/banana/third"));
+	}
+
+	TEST(GetsCorrectTemplateValues) {
+		ApiMock::RequestData request;
+		request.requestUri = "/first/banana/third";
+		ApiMock::RouteDictionary::Inject("/first/{second}/third", &request);
+		assert.are_equal("banana", request.get["second"]);
 	}
 }
