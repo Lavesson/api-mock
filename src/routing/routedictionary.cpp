087c8dc31488fe4e7a8efcb1159c3265636e603b
Eric Lavesson <eric.lavesson@gmail.com>
2014-11-05 02:41:22 +0100

    Add a route dictionary

---
+++ src/routing/routedictionary.cpp
@@ -0,0 +1,52 @@
+#include "routedictionary.h"
+#include "http/requestdata.h"
+#include <sstream>
+#include <vector>
+#include <regex>
+
+using std::vector;
+using std::string;
+using std::stringstream;
+using std::regex;
+
+/* 
+ * TODO: I get the feeling that I have the wrong structure here. Basically, I'm reusing functions from 
+ * ApiMock::RoutingTemplate. On the other hand, the injection should probably not be done in that class
+ * either. I'm suspecting that a helper class or some free helper functions might be in order instead...
+ */
+
+vector<string> splitUriIntoParts(const string& uri) {
+	vector<string> output;
+	stringstream ss(uri);
+	string next;
+
+	while (std::getline(ss, next, '/')) {
+		if (next != "") output.push_back(next);
+	}
+
+	return output;
+}
+
+bool isTemplatePart(string templatePart) {
+	return (
+		std::regex_match(templatePart, regex("\\{\\w+\\}")));
+}
+
+
+string templateName(string templatePart) {
+	std::smatch matches;
+	std::regex_match(templatePart, matches, regex("\\{(\\w+)\\}"));
+	return matches[1];
+}
+
+void ApiMock::RouteDictionary::Inject(std::string routeTemplate, RequestData* request) {
+	auto templateUriParts = splitUriIntoParts(routeTemplate);
+	auto incomingUriParts = splitUriIntoParts(request->requestUri);
+
+	for (auto i = 0; i < templateUriParts.size(); ++i) {
+		if (isTemplatePart(templateUriParts[i])) {
+			request->get.insert(
+				std::make_pair(templateName(templateUriParts[i]), incomingUriParts[i]));
+		}
+	}
+}
---
+++ src/routing/routedictionary.h
@@ -0,0 +1,16 @@
+#ifndef	APIMOCK_ROUTEDICTIONARY_H
+#define APIMOCK_ROUTEDICTIONARY_H
+
+#include <string>
+#include <unordered_map>
+
+namespace ApiMock {
+	class RequestData;
+
+	struct RouteDictionary {
+		static void Inject(std::string routeTemplate, RequestData* request);
+		std::unordered_map<std::string, std::string> get;
+	};
+}
+
+#endif
