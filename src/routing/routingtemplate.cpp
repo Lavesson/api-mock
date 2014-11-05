c73c29a930012728ccae4ad6540af2d1d0039fc7
Eric Lavesson <eric.lavesson@gmail.com>
2014-11-05 02:09:36 +0100

    Match routes on templated parts as well

--- src/routing/routingtemplate.cpp
+++ src/routing/routingtemplate.cpp
@@ -1,17 +1,64 @@
 #include "routingtemplate.h"
+#include <vector>
+#include <sstream>
+#include <regex>
+
+using std::vector;
+using std::string;
+using std::stringstream;
+using std::regex;
 
 bool ApiMock::RoutingTemplate::operator==(const RoutingTemplate& other) const {
 	return (other._routeTemplate == _routeTemplate);
 }
 
-ApiMock::RoutingTemplate::RoutingTemplate(std::string const& routeTemplate) 
-	: _routeTemplate(routeTemplate) {}
+ApiMock::RoutingTemplate::RoutingTemplate(const string& routeTemplate)
+	: _routeTemplate(routeTemplate), _routeParts(splitUriIntoParts(routeTemplate)) {}
 
-std::string ApiMock::RoutingTemplate::getTemplate() const {
+string ApiMock::RoutingTemplate::getTemplate() const {
 	return _routeTemplate;
 }
 
-bool ApiMock::RoutingTemplate::isMatch(std::string const& uri) const {
+vector<string> ApiMock::RoutingTemplate::splitUriIntoParts(const string& uri) const {
+	vector<string> output;
+	stringstream ss(uri);
+	string next;
+
+	while (std::getline(ss, next, '/')) {
+		if (next != "") output.push_back(next);
+	}
+
+	return output;
+}
+
+bool ApiMock::RoutingTemplate::numberOfArgsNotMatching(const std::vector<std::string>& uriParts) const {
+	return (uriParts.size() != _routeParts.size());
+}
+
+bool ApiMock::RoutingTemplate::isTemplatePart(std::string const& uriPart) const {
+		return (
+			std::regex_match(uriPart, regex("\\{\\w+\\}")));
+}
+
+bool ApiMock::RoutingTemplate::constraintsNotMatching(const std::vector<std::string>& uriParts) const {
+	for (auto i = 0; i < uriParts.size(); ++i) {
+		if (!isTemplatePart(_routeParts[i])) {
+			if (uriParts[i] != _routeParts[i])
+				return true;
+		}
+	}
+
+	return false;
+}
+
+bool ApiMock::RoutingTemplate::isMatch(string const& uri) const {
 	// TODO: Right now, I'm just matching exact URLs, which sucks.
-	return (uri == _routeTemplate);
+	vector<string> uriParts = splitUriIntoParts(uri);
+
+	if (numberOfArgsNotMatching(uriParts) || constraintsNotMatching(uriParts))
+		return false;
+
+	return true;
+
+	//return (uri == _routeTemplate);
 }
--- src/routing/routingtemplate.h
+++ src/routing/routingtemplate.h
@@ -2,10 +2,16 @@
 #define APIMOCK_ROUTINGTEMPLATE_H
 
 #include <string>
+#include <vector>
 
 namespace ApiMock {
 	class RoutingTemplate {
 		std::string _routeTemplate;
+		std::vector<std::string> _routeParts;
+		std::vector<std::string> splitUriIntoParts(const std::string& uri) const;
+		bool numberOfArgsNotMatching(const std::vector<std::string>& uriParts) const;
+		bool isTemplatePart(const std::string& uriPart) const;
+		bool constraintsNotMatching(const std::vector<std::string>& uriParts) const;
 
 	public:
 		bool operator==(const RoutingTemplate& other) const;
