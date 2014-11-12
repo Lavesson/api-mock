b96ce01233918450b76b370d6bcd4ee91d92a4f4
Eric Lavesson <eric.lavesson@gmail.com>
2014-11-13 00:58:53 +0100

    Return 404's when expected dashboard file missing

--- src/controllers/dashboardcontroller.cpp
+++ src/controllers/dashboardcontroller.cpp
@@ -2,13 +2,19 @@
 #include "serialization/result.h"
 #include "infrastructure/contentservice.h"
 #include <fstream>
+#include <infrastructure/exceptions.h>
 
 
 ApiMock::DashboardController::DashboardController(ContentService* content)
 	: _content(content) {}
 
 ApiMock::ResponseData ApiMock::DashboardController::get(RequestData request) {
-	auto content = _content->getContent("www/app/index.html");
-	RawResult c(content.content, content.mimeType);
-	return createResponse(HTTP_OK, &c);
+	try {
+		auto content = _content->getContent("www/app/index.html");
+		RawResult c(content.content, content.mimeType);
+		return createResponse(HTTP_OK, &c);
+	}
+	catch (FileNotFoundException e) {
+		return createResponse(HTTP_NOT_FOUND);
+	}
 }
